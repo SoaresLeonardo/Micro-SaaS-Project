63cc8e0ac3225389173082b1bbe90c7303992338
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2024-03-09 14:52:10 -0300

    Init

---
+++ src/app/auth/_components/auth-form.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useForm } from "react-hook-form";
+
+const AuthForm = () => {
+  const form = useForm();
+
+  const handleSubmit = form.handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl font-bold">Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <div className="space-y-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              placeholder="m@example.com"
+              required
+              type="email"
+              {...form.register("email")}
+            />
+          </div>
+          <Button className="w-full" type="submit">
+            Send Magic Link
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default AuthForm;
